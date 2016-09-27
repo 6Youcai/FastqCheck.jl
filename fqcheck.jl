806b90d6949826c2b012ed43bf3e592fa837359c
Youcai-at-Yuce <904731544@qq.com>
2016-09-27 10:03:35 +0800

    add GC,reads,error in fqcheck.jl

--- fqcheck.jl
+++ fqcheck.jl
@@ -10,9 +10,11 @@ function fqcheck(infile::ASCIIString, read_length::Int64)
     end
     outArray::Array{Int64,2} = zero(Array{Int64,2}(read_length,48))
     orderDict::Dict{Char,Int64} = Dict{Char,Int64}('A'=>1, 'T'=>2, 'C'=>3, 'G'=>4, 'N'=>5)
+    reads = 0
     for line1::ASCIIString in eachline(hand)
         line2::ASCIIString = readline(hand)[1:end-1]
         _::ASCIIString, line4::ASCIIString = readline(hand), readline(hand)
+        reads += 1
         len::Int64 = length(line2)
         for i in 1:len
             base::Char = line2[i]
@@ -24,7 +26,7 @@ function fqcheck(infile::ASCIIString, read_length::Int64)
             outArray[i, qualInArray]::Int64 += 1
         end
     end
-    return outArray
+    return outArray, reads
 end
 
 function ration(a::Int64,t::Int64,c::Int64,g::Int64,n::Int64)
@@ -37,20 +39,37 @@ function ration(a::Int64,t::Int64,c::Int64,g::Int64,n::Int64)
     return(ra, rt, rc, rg, rn)
 end
 
-function printArray(inArray::Array{Int64,2}, outfile::ASCIIString)
+function error_rate(inArray::Array{Int64,2})
+    all_base = sum(inArray[1:end, 1:end])
+    sum_qual = 0
+    for i in 1:43
+        tmp = sum(inArray[1:end, i])
+        sum_qual += tmp * (i-1)
+    end
+    qual = sum_qual/all_base
+    E = 10^(-1*qual/10)
+    return 100*E
+end
+
+function printArray(inArray::Array{Int64,2}, reads, outfile::ASCIIString)
     f::IOStream = open(outfile, "w")
+
     all_base = sum(inArray[1:end, 6:end])
     q20 = sum(inArray[1:end, 26:end])/all_base
     q30 = sum(inArray[1:end, 36:end])/all_base
-    print(f, "#base: ", all_base, '\n')
-    print(f, "#Q20: ", q20*100, '\n')
-    print(f, "#Q30: ", q30*100, '\n')
+    gc = sum(inArray[1:end, 3:4])/all_base
+    E = error_rate(inArray[1:end, 6:end])
+
+    println(f, "#reads: ", reads, " base: ", all_base)
+    println(f, "#Q20: ", q20*100, " Q30: ", q30*100)
+    println(f, "#GC content ", gc, " error% ", E)
 
     print(f, "pos A T C G N")
     for qual in 0:42
         print(f, " ", qual)
     end
     println(f)
+
     reads_len::Int64 = size(inArray, 1)
     for pos in 1:reads_len
         A::Int64 = inArray[pos,1]
@@ -58,7 +77,7 @@ function printArray(inArray::Array{Int64,2}, outfile::ASCIIString)
         C::Int64 = inArray[pos,3]
         G::Int64 = inArray[pos,4]
         N::Int64 = inArray[pos,5]
-        if A+T+C+G+N == 0 ## 
+        if A+T+C+G+N == 0 #
             return 0
         end
         ra::Float64, rt::Float64, rc::Float64, rg::Float64, rn::Float64 = ration(A, T, C, G, N)
@@ -68,8 +87,10 @@ function printArray(inArray::Array{Int64,2}, outfile::ASCIIString)
             print(f, " ", qual_count)
         end
         println(f)
+
     end
     close(f)
+    return 1
 end
 
 # main
@@ -82,5 +103,5 @@ end
     name1 = split(name, '.')[1]
     outfile = string(name1, ".fqcheck")
     reads_len = 151 # the longest 
-    resultArray = fqcheck(infile, reads_len)
-    printArray(resultArray, outfile)
+    resultArray, reads = fqcheck(infile, reads_len)
+    printArray(resultArray, reads, outfile)
